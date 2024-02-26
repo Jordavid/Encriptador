99b2d11383903f25cd324c4246426a8ce759a2d6
Jordavid <perez.jordavid@gmail.com>
2024-02-26 12:00:17 -0600

    Actualizacion en el metodo cifrar

--- app.js
+++ app.js
@@ -1,41 +1,35 @@
+let campoEntrada = document.querySelector('#cifrarTexto');
+let campoSalida = document.querySelector('#descifrarTexto');
 
+const matriz_code = [
+    ["e", "enter"],
+    ["i", "imes"],
+    ["a", "ai"],
+    ["o", "ober"],
+    ["u", "ufat"]
+];
 
-function cifrar(){
-    var text1 = document.getElementById('cifrarTexto');
-    var textResult = text1.value;
-    console.log(textResult);
+function cifrarT(){
+    let text = cifrar(campoEntrada.value);
+    campoSalida.value = text;
+}
 
-    /*Verifica si  el input está vacío*/
-    if(textResult == ''){
-        alert('Campo requerido');
-    }else{
-        /*Verificar si el texto contiene letras mayúsculas o caracteres especiales*/
-        if(/[A-Z]/.test(textResult) || /[^a-zA-A0-9\s]/.test(textResult)){
-            alert('No se aceptan letras mayusculas, acentos ni caracteres especiales.');
-        } else{
-            // Remplaza la vocal por otra palabra
-            var palabra = textResult.replace(/a/g,'ai');
-            palabra = palabra.replace(/e/g, 'enter');
-            palabra = palabra.replace(/i/g, 'imes');
-            palabra = palabra.replace(/o/g, 'ober');
-            palabra = palabra.replace(/u/g, 'ufat');
-
-            console.log(palabra);
-
-            var mensajeC = document.getElementById('descifrarTexto');
-            mensajeC.value=palabra;
-            limpiarText();
+function cifrar(textoEncriptar){
+    for(let i = 0; i < matriz_code.length; i++){
+        if(textoEncriptar.includes(matriz_code[i][0])){
+            textoEncriptar = textoEncriptar.replaceAll(
+                matriz_code[i][0],
+                matriz_code[i][1]
+            );
         }
     }
-
-    
-    return;
+    limpiarText();
+    return textoEncriptar;
 }
 
 function descifrar(){
     var text1 = document.getElementById('cifrarTexto');
     var textResult = text1.value;
-    console.log(textResult);
 
     if(textResult != ''){
         /*Verificar si el texto contiene letras mayúsculas o caracteres especiales*/
@@ -47,8 +41,6 @@ function descifrar(){
             palabra = palabra.replace(/imes/g, 'i');
             palabra = palabra.replace(/ober/g, 'o');
             palabra = palabra.replace(/ufat/g, 'u');
-
-            console.log(palabra);
 
             var mensajeC = document.getElementById('descifrarTexto');
             mensajeC.value=palabra;
@@ -70,10 +62,12 @@ function copiarText(){
         content.select();
         document.execCommand('copy');
         asignarTexto('p','Texto copiado al portapapeles');
+        
+       /* alert('Texto copiado al portapapeles');*/
     } else{
         alert('No hay texto que copiar');
     }
-    
+    return
 }
 
 function asignarTexto(elemento, texto){
@@ -90,11 +84,7 @@ function limpiarText(){
     document.querySelector('#cifrarTexto').value='';
 }
 
-function cifrarT(){
-    cifrar();
-}
 
 function descifrarT(){
     descifrar();
 }
-
